a8673c197d5a0697846a1ad9480a2dd733be01de
Thomas Franquelin <thomas.franquelin@spenta.biz>
2011-06-01 11:37:48 +0200

    changed server name

--- wordpress_choisirfacile/wp-config.php
+++ wordpress_choisirfacile/wp-config.php
@@ -28,7 +28,7 @@ define('DB_USER', 'choisirfaci-6665');
 define('DB_PASSWORD', 'manah');
 
 /** Adresse de l'hébergement MySQL. */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db01-share');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8');
